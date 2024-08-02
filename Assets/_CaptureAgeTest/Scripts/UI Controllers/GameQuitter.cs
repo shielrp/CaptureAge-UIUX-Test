48e9e22fb37c2a24881a117b4cb2c04457e7a893
Ryan S. (Home PC) <93ryans@gmail.com>
2024-08-02 17:33:14 -0400

    added a quit button

---
+++ Assets/_CaptureAgeTest/Scripts/UI Controllers/GameQuitter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace RPG_CharacterSelect.UI
+{
+    public class GameQuitter : MonoBehaviour
+    {
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
