b83e0db70b8c3a5c0b9fc4a7fac850f1a52a4e6d
Ryan S. (Home PC) <93ryans@gmail.com>
2024-08-02 17:14:42 -0400

    added a fade-in effect

---
+++ Assets/_CaptureAgeTest/Scripts/UI Controllers/FadeInEffect.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG_CharacterSelect.UI
+{
+    [RequireComponent(typeof(Image))]
+    public class FadeInEffect : MonoBehaviour
+    {
+
+        public float FadeTime;
+        public Color FadeColor;
+        public Ease FadeEase;
+        public bool PlayOnStart;
+
+        private Image _img;
+
+        private void Awake()
+        {
+            _img = GetComponent<Image>();
+        }
+
+        private void Start()
+        {
+            if (PlayOnStart)
+                PlayEffect();
+            else
+                _img.enabled = false;
+        }
+
+        public void PlayEffect()
+        {
+            _img.color = new Color(FadeColor.r, FadeColor.g, FadeColor.b, 1f);
+            _img.enabled = true;
+
+            DOTween.Sequence()
+                .Append(_img.DOFade(0f, FadeTime).SetEase(FadeEase))
+                .AppendCallback(() => _img.enabled = false);
+        }
+    }
+}
