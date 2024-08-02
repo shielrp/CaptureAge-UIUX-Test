7ddd95db5e9df52307d1342a6ea4db606ab39e70
Ryan S. (Home PC) <93ryans@gmail.com>
2024-08-02 16:44:13 -0400

    sounds on char select

---
+++ Assets/_CaptureAgeTest/Scripts/UI Controllers/UIButtonSoundPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG_CharacterSelect.UI
+{
+    [RequireComponent(typeof(Button))]
+    public class UIButtonSoundPlayer : MonoBehaviour
+    {
+
+        public AudioClip ButtonClickSound;
+
+        [SerializeField] private AudioSource _source;
+
+        private Button _btn;
+
+        private void Awake()
+        {
+            _btn = GetComponent<Button>();
+        }
+
+        private void OnEnable()
+        {
+            _btn.onClick.AddListener(PlaySound);
+        }
+
+        private void OnDisable()
+        {
+            _btn.onClick.RemoveListener(PlaySound);
+        }
+
+        public void PlaySound()
+        {
+            _source.PlayOneShot(ButtonClickSound);
+        }
+    }
+}
